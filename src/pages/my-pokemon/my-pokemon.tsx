c0774d16cdabdac0bc42d05a3059213c683e155c
gufranlazuardi <gufranlazuardi@gmail.com>
2024-01-13 18:35:06 +0700

    feat: fix unused variable at my pokemon

--- src/pages/my-pokemon/my-pokemon.tsx
+++ src/pages/my-pokemon/my-pokemon.tsx
@@ -4,7 +4,7 @@ const MyPokemon = () => {
   return (
     <Layout>
       <div className="grid grid-cols-2 gap-4">
-        {"123".split("").map((i) => (
+        {/* {"123".split("").map((i) => (
           <div className="w-full h-auto border rounded-md border-black flex flex-col text-center p-4">
             <img
               src="https://id.portal-pokemon.com/play/resources/pokedex/img/pm/5794f0251b1180998d72d1f8568239620ff5279c.png"
@@ -15,7 +15,7 @@ const MyPokemon = () => {
               <p className=" text-2xl">(sdafa)</p>
             </div>
           </div>
-        ))}
+        ))} */}
       </div>
     </Layout>
   );
