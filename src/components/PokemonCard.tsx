f10f3ad42a9893a9508f183fa65e942a11adb720
gufranlazuardi <gufranlazuardi@gmail.com>
2024-01-13 18:24:41 +0700

    feat: add pokemon card

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,26 +1,24 @@
 import { Pokemon } from "@/utils/apis/types";
-import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+
+import { Link } from "react-router-dom";
 
 type Props = {
   data: Pokemon;
 };
 
 const PokemonCard = (props: Props) => {
-  const navigate = useNavigate();
   const { data } = props;
   return (
-    <div
-      className="w-full h-full border rounded-md flex flex-col gap-6"
-      onClick={() => navigate(`/pokemon/detail`)}
-    >
-      <img
-        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${data.id}.svg`}
-        alt="pokemon"
-        className="p-8 h-3/4 w-auto items-center"
-      />
-      <div className=" text-center text-3xl">{data.name}</div>
-    </div>
+    <Link to={`/detail/${data.id}`}>
+      <div className="w-full h-full border rounded-md flex flex-col gap-6">
+        <img
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${data.id}.svg`}
+          alt="pokemon"
+          className="p-8 h-3/4 w-auto items-center"
+        />
+        <div className=" text-center text-3xl">{data.name}</div>
+      </div>
+    </Link>
   );
 };
 
