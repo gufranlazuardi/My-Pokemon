6f0ce3f687da86919c9ee2208dac3886fbbccb98
gufranlazuardi <gufranlazuardi@gmail.com>
2024-01-13 18:25:24 +0700

    feat: add detail pokemon pages

--- src/pages/detail-pokemon.tsx/detail-pokemon.tsx
+++ src/pages/detail-pokemon.tsx/detail-pokemon.tsx
@@ -2,25 +2,44 @@ import Layout from "@/components/Layout";
 import { Button } from "@/components/ui/button";
 import { Progress } from "@/components/ui/progress";
 import { toast } from "@/components/ui/use-toast";
-import { getPokemonDetail } from "@/utils/apis/api";
-import { Pokemon } from "@/utils/apis/types";
-import React, { useEffect, useState } from "react";
+import { getPokemonDetail, getPokemonSpecies } from "@/utils/apis/api";
+import { Pokemon, Species } from "@/utils/apis/types";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 const DetailPokemon = () => {
   const navigate = useNavigate();
 
   const [pokemon, setPokemon] = useState<Pokemon>();
+  const [species, setSpecies] = useState<Species>();
+
   const params = useParams();
 
   useEffect(() => {
     fetchData();
+    fetchDataSpecies();
   }, []);
 
   async function fetchData() {
     try {
-      const result = await getPokemonDetail(params.id_pokemon!);
+      const result = await getPokemonDetail(params.id!);
+
       setPokemon(result);
+    } catch (error: any) {
+      toast({
+        title: "Oops! Something went wrong.",
+        description: error.toString(),
+        variant: "destructive",
+      });
+    }
+  }
+
+  async function fetchDataSpecies() {
+    try {
+      const result = await getPokemonSpecies(+params.id!);
+
+      setSpecies(result);
+      console.log("ini species", result);
     } catch (error: any) {
       toast({
         title: "Oops! Something went wrong.",
@@ -34,65 +53,50 @@ const DetailPokemon = () => {
     <Layout>
       <div className="grid grid-cols-2 gap-2">
         <img
-          src="https://id.portal-pokemon.com/play/resources/pokedex/img/pm/5794f0251b1180998d72d1f8568239620ff5279c.png"
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon?.id}.svg`}
           alt="pokemon"
-          className="border rounded-lg h-full"
+          className="border rounded-lg h-full p-8"
         />
         <div className="border rounded-lg p-4 h-full">
-          <div className="flex justify-between py-2">
-            <p>Hp</p>
-            <p>60</p>
-          </div>
-          <Progress value={33} />
-          <div className="flex justify-between py-2">
-            <p>Attack</p>
-            <p>60</p>
-          </div>
-          <Progress value={89} />
-          <div className="flex justify-between py-2">
-            <p>Deffense</p>
-            <p>60</p>
-          </div>
-          <Progress value={46} />
-          <div className="flex justify-between py-2">
-            <p>Special Attack</p>
-            <p>60</p>
-          </div>
-          <Progress value={390} />
-          <div className="flex justify-between py-2">
-            <p>Special Defense</p>
-            <p>60</p>
-          </div>
-          <Progress value={70} />
-          <div className="flex justify-between py-2">
-            <p>Speed</p>
-            <p>60</p>
-          </div>
-          <Progress value={79} />
+          {pokemon?.stats.map((data, index) => (
+            <div key={index}>
+              <div className="flex justify-between py-2">
+                <p>{data.stat.name}</p>
+                <p>{data.base_stat}</p>
+              </div>
+              <Progress value={data.base_stat} />
+            </div>
+          ))}
         </div>
       </div>
 
       <div className="grid gap-2 grid-cols-2 text-center pt-6">
         <div className="border rounded-lg text-center flex flex-col gap-4 p-4">
           <h2 className=" text-2xl font-bold">Information</h2>
-          <hr className="" />
+          <hr />
           <p className="= font-semibold text-xl">Height</p>
-          <p>160 cm</p>
+          <p>{pokemon?.height} m</p>
           <p className=" font-semibold text-xl">Weight</p>
-          <p>20 kg</p>
+          <p>{pokemon?.weight} kg</p>
           <p className=" font-semibold text-xl">Habitat</p>
-          <p>Grassland</p>
+          <p>{species?.habitat.name}</p>
         </div>
         <div className="border flex flex-col gap-4 rounded-lg p-4 text-center">
           <h2 className=" text-2xl font-bold">Ability</h2>
           <hr className="" />
-          <p className="">Overgrow</p>
-          <p>Chlorophyll</p>
+          {pokemon?.abilities.map((data, index) => (
+            <p key={index}>{data.ability.name}</p>
+          ))}
         </div>
       </div>
 
       <div className="flex flex-col justify-center gap-4 py-8">
-        <Button className="flex w-full bg-green-500 shadow-md shadow-green-500">
+        <Button
+          className="flex w-full bg-green-500 shadow-md shadow-green-500"
+          onClick={() => {
+            navigate(`/catch/:${pokemon?.id}`);
+          }}
+        >
           Catch
         </Button>
         <Button
